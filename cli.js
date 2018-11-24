9f34aecadb4bf58f68f095c389393b92684fc1ef
neerajk1208@gmail.com <neerajk1208@gmail.com>
2018-11-23 17:34:33 -0800

    created init function, added questions, and initiated tracker

--- cli.js
+++ cli.js
@@ -13,15 +13,68 @@ const DEFAULT_NOTIFICATION_CONFIG = {
   wait: true,
   closeLabel: 'Close',
   actions: 'Okay!',
-  message: 'Take a second, and make a commit!',
+  message: 'Pause, and make a commit!',
 };
 
-const DEFAULT_INTERVAL = 5000;
+const DEFAULT_INTERVAL = 300000; //  5 minutes in milliseconds
 
 const CommitComrade = function () {
   this.config = DEFAULT_NOTIFICATION_CONFIG
   this.interval = DEFAULT_INTERVAL;
 };
 
+CommitComrade.prototype.getInterval = function () {
+  return this.interval;
+};
+
+CommitComrade.prototype.updateInterval = function (intervalInSeconds) {
+  const intervalInMilliseconds = intervalInSeconds * 1000;
+  this.interval = intervalInMilliseconds;
+  return;
+};
+
+CommitComrade.prototype.getConfig = function () {
+  return this.config;
+};
+
+CommitComrade.prototype.updateConfigProperty = function (key, value) {
+  if (this.config[key]) {
+    this.config[key] = value;
+  }
+  return;
+};
+
 const Helper = new CommitComrade();
-console.log('hello world: ', Helper.config);
+
+const initiateTracker = () => {
+  const currentInterval = Helper.getInterval();
+  const currentConfig = Helper.getConfig();
+  setInterval(() => {
+    notifier.notify(currentConfig, (error, response, metadata) => {
+      if (response === 'activate') {
+        exec('open -a terminal');
+      }
+    })
+  }, currentInterval);
+};
+
+const init = () => {
+  const questions = [
+    {
+      name: 'Interval',
+      type: 'input',
+      message: 'How often would you like to commit? Default interval is 5 minutes.',
+      validate: function (minutes) {
+        if (minutes.length && typeof value === 'number') {
+          const seconds = minutes * 60;
+          Helper.updateDuration(seconds)
+        }
+        initiateTracker();
+        return true;
+      }
+    }
+  ];
+  inquirer.prompt(questions)
+}
+
+init();
