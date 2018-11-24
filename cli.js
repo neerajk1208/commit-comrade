7ae38e3c61d31b19aafddc9164ec32e455ee837f
neerajk1208@gmail.com <neerajk1208@gmail.com>
2018-11-23 17:04:48 -0800

    installed necessary packages and added a gitignore

--- cli.js
+++ cli.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+const path = require('path');
+const inquirer = require('inquirer');
+const notifier = require('node-notifier');
+const term = require('terminal-kit').terminal;
+const args = require('minimist')(process.argv.slice(2));
+const { exec } = require('child_process');
