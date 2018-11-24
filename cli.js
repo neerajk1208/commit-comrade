677188951a0c24084be65874019f95c401f2492f
neerajk1208@gmail.com <neerajk1208@gmail.com>
2018-11-23 17:17:54 -0800

    added default config for notifiction and created a commit comrade constructor

--- cli.js
+++ cli.js
@@ -6,4 +6,22 @@ const notifier = require('node-notifier');
 const args = require('minimist')(process.argv.slice(2));
 const { exec } = require('child_process');
 
-console.log('hello world');
+const DEFAULT_NOTIFICATION_CONFIG = {
+  title: 'Commit Comrade',
+  subtitle: '',
+  sound: true,
+  wait: true,
+  closeLabel: 'Close',
+  actions: 'Okay!',
+  message: 'Take a second, and make a commit!',
+};
+
+const DEFAULT_INTERVAL = 5000;
+
+const CommitComrade = function () {
+  this.config = DEFAULT_NOTIFICATION_CONFIG
+  this.interval = DEFAULT_INTERVAL;
+};
+
+const Helper = new CommitComrade();
+console.log('hello world: ', Helper.config);
